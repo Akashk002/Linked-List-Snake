020ddbe3cdb11f223a0c8ea7af89d25b419c09e7
Akash <akash.katailiha120@gmail.com>
2025-01-18 00:24:36 +0530

    Using Level Service

---
+++ Linked-List-Snake/include/Level/LevelController.h
@@ -0,0 +1,28 @@
+#pragma once
+#pragma once
+
+namespace Level
+{
+	class LevelView;
+	class LevelModel;
+
+	class LevelController
+	{
+	private:
+		LevelModel* level_model;
+		LevelView* level_view;
+
+		void destroy();
+
+	public:
+		LevelController();
+		~LevelController();
+
+		void initialize();
+		void update();
+		void render();
+
+		float getCellWidth();
+		float getCellHeight();
+	};
+}
