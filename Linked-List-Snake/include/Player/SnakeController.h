c81f6cb76e611707fd2f921cd34674c6a749794c
Akash <akash.katailiha120@gmail.com>
2025-01-19 16:51:19 +0530

    Setup Snake

--- Linked-List-Snake/include/Player/SnakeController.h
+++ Linked-List-Snake/include/Player/SnakeController.h
@@ -1,7 +1,28 @@
+#pragma once
+
 namespace Player
 {
+	enum class SnakeState
+	{
+		ALIVE,
+		DEAD,
+	};
+
 	class SnakeController
 	{
+	private:
+		const int initial_snake_length = 10;
+
+		SnakeState current_snake_state;
+
+		void processPlayerInput();
+		void updateSnakeDirection();
+		void moveSnake();
+		void processSnakeCollision();
+		void handleRestart();
+		void reset();
+		void destroy();
+
 	public:
 		SnakeController();
 		~SnakeController();
@@ -9,11 +30,10 @@ namespace Player
 		void initialize();
 		void update();
 		void render();
-		void destroy();
 
-	private:
-
+		void spawnSnake();
+		void respawnSnake();
+		void setSnakeState(SnakeState state);
+		SnakeState getSnakeState();
 	};
-
-	
 }
